c30f412410caaf34db9db8140fcedc9d6d764399
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-06-28 10:59:41 +0200

    merge resolved

--- src/app/main-page/active-chat/chat-body/chat-body.component.ts
+++ src/app/main-page/active-chat/chat-body/chat-body.component.ts
@@ -54,8 +54,6 @@ export class ChatBodyComponent implements AfterViewInit {
 
   public formatDate(index: number): boolean {
     const date = new Date(this.chat.messages[index].timestamp);
-    let nextDate = null;
-    let nextDate = null;
     const now = new Date();
     const moreDateData = this.pipe.transform(date, 'EEEE, d MMMM yyyy');
     const moreNowData = this.pipe.transform(now, 'EEEE, d MMMM yyyy');
