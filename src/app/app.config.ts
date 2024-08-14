316fafd029243c61e0b54491bd6cc79c5c1340f3
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-08-14 09:11:22 +0200

    create environment folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,22 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
 import { DatePipe, registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
-import { connectStorageEmulator, getStorage } from 'firebase/storage';
+import { environment } from '../environments/environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB4VNMwgPi02CgsIe-gBiO6xRgBluHYfBE",
-  authDomain: "dabubble-bf68c.firebaseapp.com",
-  projectId: "dabubble-bf68c",
-  storageBucket: "dabubble-bf68c.appspot.com",
-  messagingSenderId: "244168784753",
-  appId: "1:244168784753:web:1bde2e27e46dc290cd94d0"
-};
+const firebaseConfig = environment.firebaseConfig;;
 
 export const appConfig: ApplicationConfig = {
   providers: [
