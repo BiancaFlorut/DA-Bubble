5c1ae40f5ce1d2744157065017f30c0ccab05746
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-10 13:08:10 +0200

    docs for editable-message component

--- src/app/main-page/active-chat/chat-body/message/editable-message/editable-message.component.ts
+++ src/app/main-page/active-chat/chat-body/message/editable-message/editable-message.component.ts
@@ -25,41 +25,72 @@ export class EditableMessageComponent {
   isHoveringOptions: boolean = false;
   userService = inject(UserService);
 
-  constructor() { 
-  }
-
+  /**
+   * Initializes the component.
+   * Creates a new instance of the Editor, sets its content to the message's text
+   * and sets the editor to the component's editor property.
+   */
   ngOnInit(): void {
     this.editor = new Editor();
     this.editor.setContent(this.message.text);
   }
 
+  /**
+   * Destroys the editor to prevent memory leaks.
+   */
   ngOnDestroy(): void {
     if (this.editor)
     this.editor.destroy();
   }
 
+  /**
+   * Focuses the editor after the view has been initialized.
+   * This is needed because the editor is only rendered after the view has been initialized.
+   */
   ngAfterViewInit(): void {
     this.editor?.commands.focus().exec();
   }
 
+  /**
+   * Cancels the editing of a message.
+   * Resets the editor's content to the original message's text and emits the closeEditEvent.
+   */
   cancelEdit() {
     this.editor?.setContent(this.message.text);
     this.closeEditEvent.emit();
   }
 
+  /**
+   * Saves the edited message and emits the closeEditEvent.
+   * The edited timestamp of the message is set to the current time.
+   */
   saveMessage() {
     this.message.editedTimestamp = Date.now();
     this.closeEditEvent.emit(this.message);
   }
 
+  /**
+   * Sets the isHoveringOptions flag to true, which enables the options container (send, edit, delete)
+   * of the message to be visible.
+   */
   hover() {
     this.isHoveringOptions = true;
   }
 
+  /**
+   * Sets the isHoveringOptions flag to false, which disables the options container (send, edit, delete)
+   * of the message to be visible.
+   */
   leave() {
     this.isHoveringOptions = false;
   }
 
+  /**
+   * Adds an emoji to the input field.
+   * @param id The id of the emoji to add.
+   * If the emoji is found in the list of available emojis, it is added to the input field.
+   * The focus is then set back to the input field.
+   */
   addEmoji(id: string) {
     const emoji = this.userService.emojis.find(emoji => emoji.id === id);
     if (emoji) {
