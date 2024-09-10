3d45197ae6b37f26808b8505dc34509d9aff72d2
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-10 11:54:45 +0200

    docs for chat service

--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, WritableSignal, inject, signal } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../../interfaces/user';
 import { FirebaseService } from '../firebase/firebase.service';
 import { Chat } from '../../models/chat.class';
@@ -14,8 +13,6 @@ export class ChatService {
   public firebase: FirebaseService = inject(FirebaseService);
 
   public chat = signal<Chat | undefined>(undefined);
-  // private chatSub: BehaviorSubject<Chat | undefined>
-  // public currentChat: Observable<Chat | undefined>;
 
   public currentPartner: User = {
     uid: '',
@@ -27,23 +24,27 @@ export class ChatService {
   public loading: WritableSignal<boolean> = signal(false);
   public newMessage = signal(true);
 
-  constructor() {
-    // this.chatSub = new BehaviorSubject<Chat | undefined>(this.chat);
-    // this.currentChat = this.chatSub.asObservable();
-  }
 
+  /**
+   * Resets the current chat to undefined and sets the newMessage signal to true
+   */
   public resetChat(): void {
     this.chat.set(undefined);
-    // this.chatSub = new BehaviorSubject<Chat | undefined>(this.chat);
-    // this.currentChat = this.chatSub.asObservable();
     this.newMessage.set(true);
   }
 
+  /**
+   * Sets the current chat to undefined
+   */
   public closeChat(): void {
     this.chat.set(undefined);
-    // this.chatSub.next(this.chat);
   }
 
+  /**
+   * Gets the direct chat with the given partner. If the chat doesn't exist, a new one is created.
+   * @param partner The user to get the direct chat with.
+   * @returns A promise that resolves to true if the chat was found or created, false otherwise.
+   */
   public async getChatWith(partner: User): Promise<boolean> {
     this.loading.set(true);
     const user = this.firebase.currentUser;
@@ -54,6 +55,11 @@ export class ChatService {
     } else return false;
   }
 
+  /**
+   * Sets the current chat with the given partner and loads the messages from the db.
+   * @param cid The id of the chat.
+   * @param partner The partner of the chat.
+   */
   public setActualChat(cid: string, partner: User): void {
     this.newMessage.set(false);
     onSnapshot(this.firebase.getDirectChatMessagesRef(cid), (collection) => {
@@ -67,6 +73,11 @@ export class ChatService {
     this.currentPartner = partner;
   }
 
+  /**
+   * Creates a new Message object from a Firestore document snapshot.
+   * @param doc The document snapshot.
+   * @returns A new Message object.
+   */
   public getMessage(doc: any) {
     const emojis = doc.data()['emojis'] as Emoji[];
     let msg = new Message(doc.data()['mid'], doc.data()['uid'], doc.data()['text'], doc.data()['timestamp'], emojis);
@@ -77,6 +88,12 @@ export class ChatService {
     return msg;
   }
 
+  /**
+   * Sets the current chat to the given chat with the given partner and messages.
+   * @param cid The id of the chat.
+   * @param partner The partner of the chat.
+   * @param msgs The messages of the chat.
+   */
   public setSubscriber(cid: string, partner: User, msgs: Message[]): void {
     let chat: Chat = new Chat(cid, [this.firebase.currentUser.uid!, partner.uid!], msgs);
     this.chat.set(chat);
@@ -84,11 +101,24 @@ export class ChatService {
     this.loading.set(false);
   }
 
+  /**
+   * Edits a message in the given direct chat.
+   * @param cid The id of the direct chat to which the message belongs.
+   * @param message The message to edit. The message's id is used to identify the message to edit, so
+   * it must be set.
+   */
   public async editMessage(cid: string, message: Message): Promise<void> {
     if (message.mid)
       await this.firebase.updateMessage(cid, message.mid, message);
   }
 
+  /**
+   * Sends a message to a user with the given uid.
+   * First, it gets the direct chat id between the current user and the given user.
+   * Then it sends the message to the direct chat and updates the message in the direct chat.
+   * @param uid The id of the user to which the message should be sent.
+   * @param message The text of the message to be sent.
+   */
   public async sendMessageToUser(uid: string, message: string): Promise<void> {
     if (uid.length == 0) {
       console.log("no user to send message to");
