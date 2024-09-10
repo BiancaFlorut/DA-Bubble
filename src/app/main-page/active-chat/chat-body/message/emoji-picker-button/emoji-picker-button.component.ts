42f34d8e02f16e03136296dbf833b9c90cc603c6
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-09-10 13:05:28 +0200

    docs for emoji-picker component

--- src/app/main-page/active-chat/chat-body/message/emoji-picker-button/emoji-picker-button.component.ts
+++ src/app/main-page/active-chat/chat-body/message/emoji-picker-button/emoji-picker-button.component.ts
@@ -15,16 +15,31 @@ export class EmojiPickerButtonComponent {
   userService = inject(UserService);
   emojis = this.userService.emojis;
 
+  /**
+   * Event handler for when an emoji is selected from the picker.
+   * The event is stopped from propagating, the picker is closed, and the selected emoji's id is emitted.
+   * @param id The id of the selected emoji.
+   * @param event The event that triggered this function.
+   */
   selectEmoji(id: string, event: Event) {
     event.stopPropagation();
     this.isEmojiPickerOpen = false;
     this.emojiPickerEvent.emit(id);
   }
 
+  /**
+   * Toggles the emoji picker open or closed.
+   * If the picker is currently open, it is closed, and if it is currently closed, it is opened.
+   */
   toggleEmojiPicker() {
     this.isEmojiPickerOpen = !this.isEmojiPickerOpen;
   }
 
+  /**
+   * Closes the emoji picker if it is open.
+   * Stopping propagation of the event that triggered this function.
+   * @param event The event that triggered this function.
+   */
   closeEmojiPicker(event: Event) {
     event.stopPropagation();
     this.isEmojiPickerOpen = false;
